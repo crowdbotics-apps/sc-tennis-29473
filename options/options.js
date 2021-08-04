076507f756daafe63f0e00a7b5dc1da378db2e37
Crowdbotics <team@crowdbotics.com>
2021-08-04 19:52:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sc_tennis_29473Identifier",
+  url: "https://sc_tennis_29473Identifier.botics.co",
+  api: "https://sc_tennis_29473Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
